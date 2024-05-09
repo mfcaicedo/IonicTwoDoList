e2d769230edadfa042d77c463cfaee72a5d73a9a
mfcaicedo <mfcaicedo@unicauca.edu.co>
2024-05-08 23:59:41 -0500

    feat: Se agrega el componente IonModal y se utiliza para crear tareas. Además se soluciona el error en los íconos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,46 +1,60 @@
 import { Component, ViewChild } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons, IonLabel, IonItem, IonList, IonIcon, IonInput,
-  IonItemSliding, IonItemOptions, IonItemOption,
+import {
+  IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons, IonLabel, IonItem, IonList, IonIcon, IonInput,
+  IonItemSliding, IonItemOptions, IonItemOption, IonModal,
 } from '@ionic/angular/standalone';
 import { Task } from '../interfaces/models/task.model';
 import { NgFor } from '@angular/common';
 import { NgClass } from '@angular/common';
-import { IonModal } from '@ionic/angular';
+// import { IonModal } from '@ionic/angular';
 import { OverlayEventDetail } from '@ionic/core/components';
+import { addIcons } from 'ionicons';
+import { trash } from 'ionicons/icons';
+import { add } from 'ionicons/icons';
+import { checkmark } from 'ionicons/icons';
+import { FormsModule } from '@angular/forms';
 
+/**
+ * Tarea: Agregar un Componente de Ionic 
+ * Solución: 
+ * 1- Se agrega el componente IonModal y se utilizó para crear una nueva tarea que se agrega a la lista 
+ * de tareas. 
+ * 2- Se soluciona el error en los iconos de la lista de tareas, se importa la función addIcons de ionicons
+ */
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [ NgFor,NgClass, IonInput, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons, IonLabel, 
-    IonItem, IonList, IonIcon, IonIcon, IonItemSliding, IonItemOptions, IonItemOption
+  imports: [NgFor, NgClass, IonModal, IonInput, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons, IonLabel,
+    IonItem, IonList, IonIcon, IonIcon, IonItemSliding, IonItemOptions, IonItemOption, FormsModule
   ],
 })
 export class HomePage {
 
   tasks: Array<Task> = [
     {
-      title: 'Task 1',
+      title: 'Tarea 1',
       description: 'This is a sample task',
       status: 'open'
     }, {
-      title: 'Task 2',
+      title: 'Tarea 2',
       description: 'This is a sample task',
       status: 'open'
     }, {
-      title: 'Task 3',
+      title: 'Tarea 3',
       description: 'This is a sample task',
       status: 'open'
     }
   ]
 
-  @ViewChild(IonModal) modal: IonModal | undefined;
+  @ViewChild(IonModal) modal!: IonModal;
 
-  message = 'This modal example uses triggers to automatically open a modal when the button is clicked.';
-  name = 'World';
+  taskTitle = '';
 
-  constructor() { }
+  constructor() {
+    addIcons({ trash, add, checkmark });
+  }
 
   addTask() {
     let newTask = prompt('Nueva tarea', '');
@@ -58,13 +72,24 @@ export class HomePage {
   }
 
   confirm() {
-    this.modal?.dismiss(this.name, 'confirm');
+
+    this.modal?.dismiss(this.taskTitle, 'confirm');
+
+    if (this.taskTitle !== '') {
+      this.tasks.push({
+        title: this.taskTitle,
+        description: 'This is a sample task',
+        status: 'open'
+      });
+      this.taskTitle = '';
+    }
+
   }
 
   onWillDismiss(event: Event) {
     const ev = event as CustomEvent<OverlayEventDetail<string>>;
     if (ev.detail.role === 'confirm') {
-      this.message = `Hello, ${ev.detail.data}!`;
+      // this.message = `Hello, ${ev.detail.data}!`;
     }
   }
 
@@ -81,7 +106,5 @@ export class HomePage {
       slidingItem.close();
     }, 100);
   }
-
-  //TODO: Agregar un componente cualquiera para mejorar la app que llevamos, un modal,un toggle, select, etc.
 
 }
